e9a33c651fee726383c24e29ab3efee984957a90
Duke Yin <yduke@hotmail.com>
2020-04-02 00:23:33 +0800

    Create toolbox-by-dukeyin.php

---
+++ toolbox-by-dukeyin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package Spam Protection Some Chinese Please
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Spam Protection Some Chinese Please
+Plugin URI: http://www.dukeyin.com/about/
+Description: This will need visitors to type Chinese to submit comments, against spamers.
+Author: DukeYin
+Version: 1.0.0
+Author URI: http://www.dukeyin.com/
+*/
+
+function refused_spam_comments( $comment_data ) {  
+$pattern = '/[一-龥]/u';  
+if(!preg_match($pattern,$comment_data['comment_content'])) {  
+wp_die( __('Need chinese charactors to submit your comment.','chinese-comment-only') );  
+}  
+return( $comment_data );  
+}  
+add_filter('preprocess_comment','refused_spam_comments');
+
+function duke_comment_add_at( $comment_text, $comment = '') {
+  if( $comment->comment_parent > 0) {
+    $comment_text = '@<a href="#comment-' . $comment->comment_parent . '">'.get_comment_author( $comment->comment_parent ) . '</a> ' . $comment_text;
+  }
+
+  return $comment_text;
+}
+add_filter( 'comment_text' , 'duke_comment_add_at', 20, 2);
